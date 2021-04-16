75d4aeec58aef842787eb71225724e172a2316f7
juliamsmith <40371272+juliamsmith@users.noreply.github.com>
2021-04-16 10:14:03 -0700

    Made app

--- app.R
+++ app.R
@@ -1,1 +1,56 @@
-#comment
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+spooky <- read_csv("spooky_data.csv")
+
+ui <- fluidPage(
+  titlePanel("I am adding a title!"),
+  sidebarLayout(
+    sidebarPanel("put my widgets here",
+                 selectInput(inputId = "state_select",
+                             label = "Choose a state",
+                             choices = unique(spooky$state)
+                 ),
+                 radioButtons(inputId = "region_select",
+                              label = "Choose a region",
+                              choices = unique(spooky$region_us_census))
+    ),
+    mainPanel("put my outputs here",
+              p("State's top candies:"),
+              tableOutput(outputId = "candy_table"),
+              p("costume region:"),
+              plotOutput(outputId = "costume_graph")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  state_candy <- reactive({
+    spooky %>%
+      filter(state == input$state_select) %>%
+      select(candy, pounds_candy_sold)
+  })
+  
+  output$candy_table <- renderTable({
+    state_candy()
+  })
+  
+  region_costume <- reactive({
+    spooky %>%
+      filter(region_us_census == input$region_select) %>%
+      count(costume, rank)
+  })
+  
+  
+  output$costume_graph <- renderPlot({
+    ggplot(region_costume(), aes(x = costume, y = n)) +
+      geom_col(aes(fill = rank)) +
+      coord_flip() +
+      scale_fill_manual(values = c("black","purple","orange")) +
+      theme_minimal()
+  })
+  
+}
+shinyApp(ui = ui, server = server)
